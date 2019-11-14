c4b80ddfa20e1d05b846dceae905d121d94338d1
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2019-11-14 16:24:21 +0800

    config服务

--- z-guide-config-server/src/main/java/com/yhc/example/zguide/config/server/ZGuideConfigServerApplication.java
+++ z-guide-config-server/src/main/java/com/yhc/example/zguide/config/server/ZGuideConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.yhc.example.zguide.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ZGuideConfigServerApplication {
 
     public static void main(String[] args) {
