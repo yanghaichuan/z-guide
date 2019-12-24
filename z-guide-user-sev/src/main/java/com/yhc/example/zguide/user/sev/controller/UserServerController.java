9fc5f43fccf12292bf32d9a7482acca4e5fa432c
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2019-12-24 16:25:17 +0800

    springcloud 添加 bus

--- z-guide-user-sev/src/main/java/com/yhc/example/zguide/user/sev/controller/UserServerController.java
+++ z-guide-user-sev/src/main/java/com/yhc/example/zguide/user/sev/controller/UserServerController.java
@@ -6,6 +6,7 @@ import com.yhc.example.zguide.user.api.server.UserHystrixServer;
 import com.yhc.example.zguide.user.sev.server.UserServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class UserServerController implements UserHystrixServer {
-    @Value("${spring.profiles.active}")
-    private String active;
+    @Value("${config.hello}")
+    private String hello;
 
     @Autowired
     private UserServer userServer;
@@ -27,7 +29,7 @@ public class UserServerController implements UserHystrixServer {
 
     @RequestMapping(value = "/user/test", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
     public String test() {
-        return active;
+        return hello;
     }
 
     @RequestMapping(value = "/user/list", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
