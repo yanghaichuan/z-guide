3559ef81d685bbc9bb2382aa9baabfd4714c9b4d
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2020-01-07 19:23:13 +0800

    添加文件服务

---
+++ z-guide-auth-server/src/test/java/com/yhc/example/zguide/auth/server/ZGuideAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhc.example.zguide.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZGuideAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ z-guide-config-client/src/test/java/com/yhc/example/zguide/config/client/ZGuideConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhc.example.zguide.config.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZGuideConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
