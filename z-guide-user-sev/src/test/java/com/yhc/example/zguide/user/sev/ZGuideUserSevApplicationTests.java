cf88c51e97d5b267f0511da1c5a1df211fbf3693
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2020-01-07 19:24:13 +0800

    用户模块测试类

---
+++ z-guide-user-sev/src/test/java/com/yhc/example/zguide/user/sev/ZGuideUserSevApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yhc.example.zguide.user.sev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZGuideUserSevApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
